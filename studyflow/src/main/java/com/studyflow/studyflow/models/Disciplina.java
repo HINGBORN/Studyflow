0ccf7710ab53f6bbec0caf12d41e81208a8829a7
Arthur Beraldo <arthurberaldo18@gmail.com>
2026-04-16 13:55:05 -0300

    testando fazer

--- studyflow/src/main/java/com/studyflow/studyflow/models/Disciplina.java
+++ studyflow/src/main/java/com/studyflow/studyflow/models/Disciplina.java
@@ -1,5 +1,19 @@
 package com.studyflow.studyflow.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "disciplinas")
 public class Disciplina {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 }
